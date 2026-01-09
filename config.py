4ff03b212c3baaab6201603c19cfa32414159a23
Denis Dudnikov <goldkazyna@gmail.com>
2026-01-09 16:57:57 +0500

    change payment type to card

--- config.py
+++ config.py
@@ -2,7 +2,7 @@
 API_KEY = "679e0daeba1c45d2b38ad8e3e61de32d"
 ORG_ID = "fe680633-a3d5-4e1a-a376-3b11969dad92"
 TERMINAL_ID = "7641870b-66fb-01b4-018c-8af7de070064"
-CASH_PAYMENT = "09322f46-578a-d210-add7-eec222a08871"
+CASH_PAYMENT = "e46b4e6c-10d5-a739-8fb1-b6674d1e65e7"
 
 # iiko API URL
 IIKO_API_URL = "https://api-ru.iiko.services/api/1"
